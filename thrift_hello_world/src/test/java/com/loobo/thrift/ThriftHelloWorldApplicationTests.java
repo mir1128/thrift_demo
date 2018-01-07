886285958141a729f51e7f84db3b0c8691c7bf8b
liujie <jieliu@thoughtworks.com>
2018-01-07 12:45:19 +0800

    first commit.

---
+++ thrift_hello_world/src/test/java/com/loobo/thrift/ThriftHelloWorldApplicationTests.java
@@ -0,0 +1,16 @@
+package com.loobo.thrift;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThriftHelloWorldApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
